605f5592ef5bc8bfac896f2246223183d5f8ea40
Dev-Zual <freelancerzual@gmail.com>
2022-04-18 17:19:36 +0600

    added about me content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>this is about</h2>
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col-md-12">
+          <h2 className="text-center text-primary">Something About Me!!</h2>
+          <p className="w-50 mx-auto fw-fw-normal">
+            Hello, there! I'm a frontend React web application developer. Also,
+            I have full-stack knowledge. I have expertise in React, Node Js,
+            MongoDb, express, Javascript, HTML, CSS, Bootstrap, Tailwind,
+            Material UI, etc.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
